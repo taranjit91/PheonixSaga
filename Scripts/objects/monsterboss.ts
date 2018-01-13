b2eb2284dd788accbac31d8c962afb8f14b6804f
Sean (Sangbeom) Yi <isuryun@gmail.com>
2018-01-13 18:59:17 -0500

    Add Monster Boss Pattern

--- Scripts/objects/monsterboss.ts
+++ Scripts/objects/monsterboss.ts
@@ -11,6 +11,10 @@ module objects {
     private _dx: number;
     private _dxF: boolean = true; // distinguish +-
 
+    private _2ndMovement: boolean = false;
+    private _2ndTempSwitch: boolean = false;
+    private _3rdTempSwitch: boolean = false;
+
     // PUBLIC PROPERTIES
     public bulletSpawn:createjs.Point;
     public powerBulletSpawn:createjs.Point;
@@ -85,7 +89,7 @@ module objects {
       this._bulletTriggerPoint = 100;
     }
 
-    private _updatePosition():void {
+    private _updatePosition(): void {
       if (this._dxF) {
         this.x += this._dx;
       } else {
@@ -101,16 +105,54 @@ module objects {
       this.position.y = this.y;
     }
 
+    private _updatePosition2nd(): void {
+      if(this._3rdTempSwitch == false){
+        this.x += 5;
+      }
+      else {
+        this.x -= 5;
+      }      
+
+      if(this._2ndTempSwitch == false) {
+        if(this.position.x > (config.Screen.WIDTH * 1.5)){
+          this.x = 0 - this.width;
+          this._2ndTempSwitch = true;
+        }
+      }
+      else {        
+        if(this._3rdTempSwitch == true){
+          if(this.position.x < -(config.Screen.WIDTH * 0.5)){
+            this.x = config.Screen.WIDTH + this.width;
+            this._2ndTempSwitch = false;
+            this._3rdTempSwitch = false;
+
+            this._2ndMovement = false;
+          }
+        }
+        else {
+          if(this.position.x > (config.Screen.WIDTH * 0.5)){
+            this._3rdTempSwitch = true;
+          }
+        }
+      }
+
+      this.position.x = this.x;
+    }
+
     public Damaged(bulletType:number):void {
       if(bulletType == 1)
       {
-        this._life= this._life-2;
+        this._life = this._life - 2;
       }
       else 
       {
-        this._life= this._life-1;
+        this._life = this._life - 1;
       }
       this._hitTime = createjs.Ticker.getTime();
+
+      if((this._life / 4) == 0) {
+        this._2ndMovement = true;
+      }
 
       if (this._life <= 0)
       {
@@ -125,8 +167,14 @@ module objects {
       this.powerBulletSpawn.x = this.x;
       this.powerBulletSpawn.y = this.y + 20;
 
-      this._updatePosition();
-      this._checkBounds();
+      if(this._2ndMovement == true) {
+        this._updatePosition2nd();
+      }
+      else {
+        this._updatePosition();
+        this._checkBounds();
+      }
+      
       
       this._bulletTriggerCount++;
       
@@ -155,6 +203,14 @@ module objects {
 
     public TriggerFire():boolean {
       return this._bulletTrigger;
+    }
+
+    public Set2ndMovement(changeMovement: boolean): void {
+      this._2ndMovement = changeMovement;
+    }
+
+    public Get2ndMovement(): boolean {
+      return this._2ndMovement;
     }
   }
 }
